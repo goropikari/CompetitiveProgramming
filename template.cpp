55af4834b02975f429adb3864f2108cd92c38f8b
goropikari <goropikari56@gmail.com>
2021-12-28 09:20:57 +0900

    update template

--- template.cpp
+++ template.cpp
@@ -10,10 +10,22 @@ using P = pair<int,int>;
 
 const ll INF = (ll)1e18;
 // const int INF = (int)1e9+7;
+// const int MOD = (int)1e9+7;
+// const ll MOD = (ll)1e9+7;
 template<typename T>
 void chmin(T &a, T b) { a = min(a, b); }
 template<typename T>
 void chmax(T &a, T b) { a = max(a, b); }
+
+template<typename T>
+void print(vector<T> v) {
+    int n = v.size();
+    rep(i,n) {
+        if (i == 0) cout << v[i];
+        else cout << ' ' << v[i];
+    }
+    cout << endl;
+}
 
 void solve() {
 }
