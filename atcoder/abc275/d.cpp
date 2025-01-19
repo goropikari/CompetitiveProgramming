13d3c4faa114f25e7270de35bb3c6849b05d8d40
goropikari <goropikari56@gmail.com>
2025-01-20 01:16:15 +0900

    add codes

---
+++ atcoder/abc275/d.cpp
@@ -0,0 +1,71 @@
+/*https://atcoder.jp/contests/abc275/tasks/abc275_d*/
+/*2025年01月20日 00時51分25秒*/
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+#include <bits/stdc++.h>
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rep(i, n) for (long long int i = 0; i < (n); ++i)
+#define rep2(i, k, n) for (long long int i = (k); i < (n); ++i)
+using namespace std;
+using ll = long long;
+using vint = vector<int>;
+using vll = vector<ll>;
+using vvint = vector<vector<int>>;
+using vvll = vector<vector<ll>>;
+
+// const ll INF = (ll)2e18+9;
+const int INF = (int)2e9 + 7;
+
+template <typename T>
+void chmin(T& a, T b) {
+    a = min(a, b);
+}
+template <typename T>
+void chmax(T& a, T b) {
+    a = max(a, b);
+}
+
+template <typename T>
+void print(vector<T> v) {
+    int n = v.size();
+    rep(i, n) {
+        if (i == 0)
+            cout << v[i];
+        else
+            cout << ' ' << v[i];
+    }
+    cout << endl;
+}
+
+void yesno(bool x) {
+    puts(x ? "Yes" : "No");
+}
+
+void solve();
+
+int main() {
+    solve();
+    return 0;
+}
+
+map<ll, ll> memo;
+
+ll f(ll x) {
+    if (x == 0)
+        return 1;
+
+    if (memo.count(x))
+        return memo[x];
+
+    return memo[x] = f(x / 2) + f(x / 3);
+}
+
+void solve() {
+    ll N;
+    cin >> N;
+
+    cout << f(N) << endl;
+}
