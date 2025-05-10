93d765b1e58b00ff038236a2d728240738509c35
goropikari <goropikari56@gmail.com>
2025-05-11 02:50:48 +0900

    add abc405

---
+++ atcoder/abc405/b.cpp
@@ -0,0 +1,88 @@
+// https://atcoder.jp/contests/abc405/tasks/abc405_b
+// 2025年05月10日 21時01分18秒
+#include <bits/stdc++.h>
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rep(i, n) for (long long int i = 0; i < (n); ++i)
+#define rep2(i, k, n) for (long long int i = (k); i < (n); ++i)
+using namespace std;
+using ll = long long;
+using vint = vector<int>;
+using vll = vector<ll>;
+using vvint = vector<vector<int>>;
+using vvll = vector<vector<ll>>;
+
+// const ll INF = (ll)2e18+9;
+const int INF = (int)2e9 + 7;
+
+template <typename T>
+void chmin(T& a, T b) {
+    a = min(a, b);
+}
+template <typename T>
+void chmax(T& a, T b) {
+    a = max(a, b);
+}
+
+template <typename T>
+void print(vector<T> v) {
+    int n = v.size();
+    rep(i, n) {
+        if (i == 0)
+            cout << v[i];
+        else
+            cout << ' ' << v[i];
+    }
+    cout << endl;
+}
+
+void yesno(bool x) {
+    cout << (x ? "Yes" : "No") << '\n';
+}
+
+void solve();
+
+int main() {
+    solve();
+    return 0;
+}
+
+void solve() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll N, M;
+    cin >> N >> M;
+    vll A(N);
+    rep(i, N) {
+        cin >> A[i];
+        A[i]--;
+    }
+
+    vint cnt(M);
+    for (ll a : A)
+        cnt[a]++;
+
+    int sum = 0;
+    rep(i, M) {
+        if (cnt[i] > 0)
+            sum++;
+    }
+
+    ll ans = 0;
+    rep(i, N) {
+        if (sum != M)
+            break;
+        ans++;
+        ll b = A.back();
+        A.pop_back();
+        cnt[b]--;
+        if (cnt[b] == 0)
+            sum--;
+    }
+    cout << ans << endl;
+}
---
+++ atcoder/abc405/d.cpp
@@ -0,0 +1,121 @@
+// https://atcoder.jp/contests/abc405/tasks/abc405_d
+// 2025年05月10日 21時07分45秒
+#include <bits/stdc++.h>
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rep(i, n) for (long long int i = 0; i < (n); ++i)
+#define rep2(i, k, n) for (long long int i = (k); i < (n); ++i)
+using namespace std;
+using ll = long long;
+using vint = vector<int>;
+using vll = vector<ll>;
+using vvint = vector<vector<int>>;
+using vvll = vector<vector<ll>>;
+
+// const ll INF = (ll)2e18+9;
+const int INF = (int)2e9 + 7;
+
+template <typename T>
+void chmin(T& a, T b) {
+    a = min(a, b);
+}
+template <typename T>
+void chmax(T& a, T b) {
+    a = max(a, b);
+}
+
+template <typename T>
+void print(vector<T> v) {
+    int n = v.size();
+    rep(i, n) {
+        if (i == 0)
+            cout << v[i];
+        else
+            cout << ' ' << v[i];
+    }
+    cout << endl;
+}
+
+void yesno(bool x) {
+    cout << (x ? "Yes" : "No") << '\n';
+}
+
+void solve();
+
+int main() {
+    solve();
+    return 0;
+}
+
+void solve() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll H, W;
+    cin >> H >> W;
+    vector<string> grid(H);
+    rep(i, H) cin >> grid[i];
+
+    int si = 0, sj = 0;
+    vector<pair<int, int>> exits;
+    rep(i, H) rep(j, W) {
+        if (grid[i][j] == 'E')
+            exits.push_back({i, j});
+    }
+
+    // i,j, cost
+    vvll dist(H, vll(W, INF));
+    deque<tuple<ll, ll, ll>> deq;
+    for (auto [i, j] : exits) {
+        deq.push_back({i, j, 0});
+        dist[i][j] = 0;
+    }
+
+    vll di = {0, 1, 0, -1};
+    vll dj = {1, 0, -1, 0};
+    while (deq.size()) {
+        auto [nowi, nowj, cost] = deq.front();
+        deq.pop_front();
+
+        if (dist[nowi][nowj] < cost)
+            continue;
+
+        rep(dir, 4) {
+            ll ni = nowi + di[dir], nj = nowj + dj[dir];
+            if (clamp(ni, 0ll, H - 1) != ni || clamp(nj, 0ll, W - 1) != nj)
+                continue;
+            if (grid[ni][nj] == '#')
+                continue;
+            if (dist[ni][nj] <= cost + 1)
+                continue;
+            dist[ni][nj] = cost + 1;
+            deq.push_back({ni, nj, dist[ni][nj]});
+        }
+    }
+
+    vector<char> dir_mark = {'>', 'v', '<', '^'};
+
+    vector<string> ans = grid;
+
+    rep(i, H) {
+        rep(j, W) {
+            if (grid[i][j] == '#' || grid[i][j] == 'E')
+                continue;
+            rep(dir, 4) {
+                ll ni = i + di[dir], nj = j + dj[dir];
+                if (clamp(ni, 0ll, H - 1) != ni || clamp(nj, 0ll, W - 1) != nj)
+                    continue;
+                if (dist[i][j] == dist[ni][nj] + 1) {
+                    ans[i][j] = dir_mark[dir];
+                }
+            }
+        }
+    }
+
+    for (auto s : ans)
+        cout << s << endl;
+}
