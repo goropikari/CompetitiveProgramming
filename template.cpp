f630c3a3840271f9c34810db155e441cd4b52e69
goropikari <goropikari56@gmail.com>
2025-05-23 01:22:43 +0900

    update template

--- template.cpp
+++ template.cpp
@@ -10,6 +10,7 @@ using int128 = int128_t;
 #define rall(v) (v).rbegin(), (v).rend()
 #define rep(i, n) for (long long int i = 0; i < (n); ++i)
 #define rep2(i, k, n) for (long long int i = (k); i < (n); ++i)
+#define repinc(i, n, inc) for (long long int i = (k); i < (n); ++(inc))
 #define OUTSIDE(i, j, h, w) (((i) < 0) || ((i) >= (h)) || ((j) < 0) || ((j) >= (w)))
 using namespace std;
 using ll = long long;
@@ -18,7 +19,7 @@ using vll = vector<ll>;
 using vvint = vector<vector<int>>;
 using vvll = vector<vector<ll>>;
 
-const ll INF = (ll)2e18+9;
+// const ll INF = (ll)2e18 + 9;
 // const int INF = (int)2e9 + 7;
 
 template <typename T>
