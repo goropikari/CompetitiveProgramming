aae4de0a28537b86f2f920065c0384326813c1ac
goropikari <goropikari56@gmail.com>
2025-05-26 02:17:00 +0900

    add codes

--- atcoder/arc198/a.cpp
+++ atcoder/arc198/a.cpp
@@ -1,8 +1,12 @@
+// https://atcoder.jp/contests/arc198/tasks/arc198_a
+// 2025年05月25日 23時27分32秒
 #include <bits/stdc++.h>
-// #include <atcoder/all>
+#include <atcoder/all>
 // using namespace atcoder;
 // using mint = modint998244353;
 // using mint = modint1000000007;
+// modint::set_mod(10);
+// using mint = modint;
 #include <boost/multiprecision/cpp_int.hpp>
 using namespace boost::multiprecision;
 using int128 = int128_t;
@@ -10,7 +14,7 @@ using int128 = int128_t;
 #define rall(v) (v).rbegin(), (v).rend()
 #define rep(i, n) for (long long int i = 0; i < (n); ++i)
 #define rep2(i, k, n) for (long long int i = (k); i < (n); ++i)
-#define repinc(i, n, inc) for (long long int i = (k); i < (n); ++(inc))
+#define repinc(i, n, inc) for (long long int i = (k); i < (n); i += (inc))
 #define OUTSIDE(i, j, h, w) (((i) < 0) || ((i) >= (h)) || ((j) < 0) || ((j) >= (w)))
 using namespace std;
 using ll = long long;
@@ -19,7 +23,7 @@ using vll = vector<ll>;
 using vvint = vector<vector<int>>;
 using vvll = vector<vector<ll>>;
 
-// const ll INF = (ll)2e18 + 9;
+// const ll INF = (ll)2e18+9;
 // const int INF = (int)2e9 + 7;
 
 template <typename T>
@@ -47,14 +51,6 @@ void yesno(bool x) {
     cout << (x ? "Yes" : "No") << '\n';
 }
 
-void Yes() {
-    yesno(true);
-}
-
-void No() {
-    yesno(false);
-}
-
 void solve();
 
 int main() {
@@ -65,4 +61,21 @@ int main() {
 void solve() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    if (N == 1) {
+        cout << 1 << endl;
+        cout << 1 << endl;
+        return;
+    }
+
+    vint ans;
+    for (int i = 2; i <= N; i += 2) {
+        ans.push_back(i);
+    }
+
+    cout << ans.size() << endl;
+    print(ans);
 }
--- atcoder/arc198/b.cpp
+++ atcoder/arc198/b.cpp
@@ -1,8 +1,12 @@
+// https://atcoder.jp/contests/arc198/tasks/arc198_b
+// 2025年05月26日 00時06分52秒
 #include <bits/stdc++.h>
-// #include <atcoder/all>
+#include <atcoder/all>
 // using namespace atcoder;
 // using mint = modint998244353;
 // using mint = modint1000000007;
+// modint::set_mod(10);
+// using mint = modint;
 #include <boost/multiprecision/cpp_int.hpp>
 using namespace boost::multiprecision;
 using int128 = int128_t;
@@ -10,7 +14,7 @@ using int128 = int128_t;
 #define rall(v) (v).rbegin(), (v).rend()
 #define rep(i, n) for (long long int i = 0; i < (n); ++i)
 #define rep2(i, k, n) for (long long int i = (k); i < (n); ++i)
-#define repinc(i, n, inc) for (long long int i = (k); i < (n); ++(inc))
+#define repinc(i, n, inc) for (long long int i = (k); i < (n); i += (inc))
 #define OUTSIDE(i, j, h, w) (((i) < 0) || ((i) >= (h)) || ((j) < 0) || ((j) >= (w)))
 using namespace std;
 using ll = long long;
@@ -19,7 +23,7 @@ using vll = vector<ll>;
 using vvint = vector<vector<int>>;
 using vvll = vector<vector<ll>>;
 
-// const ll INF = (ll)2e18 + 9;
+// const ll INF = (ll)2e18+9;
 // const int INF = (int)2e9 + 7;
 
 template <typename T>
@@ -47,14 +51,6 @@ void yesno(bool x) {
     cout << (x ? "Yes" : "No") << '\n';
 }
 
-void Yes() {
-    yesno(true);
-}
-
-void No() {
-    yesno(false);
-}
-
 void solve();
 
 int main() {
@@ -65,4 +61,23 @@ int main() {
 void solve() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    rep(_, t) {
+        ll x, y, z;
+        cin >> x >> y >> z;
+
+        if (x < z) {
+            yesno(false);
+            continue;
+        };
+
+        if (y <= 2 * (x - z)) {
+            y %= 2;
+        } else {
+            y -= 2 * (x - z);
+        }
+        yesno(y <= z * 2);
+    }
 }
