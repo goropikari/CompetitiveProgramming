b815bda0513a82ffdf3b9292ebccf5903df842ec
goropikari <goropikari56@gmail.com>
2025-05-27 03:01:38 +0900

    add codes

---
+++ atcoder/arc116/b.cpp
@@ -0,0 +1,92 @@
+// https://atcoder.jp/contests/arc116/tasks/arc116_b
+// 2025年05月27日 01時14分51秒
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace atcoder;
+using mint = modint998244353;
+// using mint = modint1000000007;
+// modint::set_mod(10);
+// using mint = modint;
+#include <boost/multiprecision/cpp_int.hpp>
+using namespace boost::multiprecision;
+using int128 = int128_t;
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rep(i, n) for (long long int i = 0; i < (n); ++i)
+#define rep2(i, k, n) for (long long int i = (k); i < (n); ++i)
+#define repinc(i, n, inc) for (long long int i = (k); i < (n); i += (inc))
+#define OUTSIDE(i, j, h, w) (((i) < 0) || ((i) >= (h)) || ((j) < 0) || ((j) >= (w)))
+using namespace std;
+using ll = long long;
+using vint = vector<int>;
+using vll = vector<ll>;
+using vvint = vector<vector<int>>;
+using vvll = vector<vector<ll>>;
+
+// const ll INF = (ll)2e18+9;
+// const int INF = (int)2e9 + 7;
+
+template <typename T>
+void chmin(T& a, T b) {
+    a = min(a, b);
+}
+template <typename T>
+void chmax(T& a, T b) {
+    a = max(a, b);
+}
+
+template <typename T>
+void print(vector<T> v) {
+    int n = v.size();
+    rep(i, n) {
+        if (i == 0)
+            cout << v[i];
+        else
+            cout << ' ' << v[i];
+    }
+    cout << endl;
+}
+
+void yesno(bool x) {
+    cout << (x ? "Yes" : "No") << '\n';
+}
+
+void Yes() {
+    yesno(true);
+}
+
+void No() {
+    yesno(false);
+}
+
+void solve();
+
+int main() {
+    solve();
+    return 0;
+}
+
+void solve() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+    vll A(N);
+    rep(i, N) cin >> A[i];
+    sort(all(A));
+
+    mint ans = 0;
+    mint sum = 0;
+    for (int i = N - 2; i >= 0; i--) {
+        sum *= 2;
+        sum += A[i + 1];
+        ans += sum * A[i];
+    }
+
+    rep(i, N) {
+        ans += (mint)A[i] * A[i];
+    }
+
+    cout << ans.val() << endl;
+}
