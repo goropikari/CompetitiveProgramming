53e24e6dad971ad05b2383d14ee923d3245a337d
goropikari <goropikari56@gmail.com>
2025-07-13 13:05:26 +0900

    add codes

---
+++ atcoder/arc162/a.cpp
@@ -0,0 +1,108 @@
+// https://atcoder.jp/contests/arc162/tasks/arc162_a
+// 2025年07月13日 02時33分57秒
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+// using vmint = vector<mint>;
+// modint::set_mod(10);
+// using mint = modint;
+#include <boost/multiprecision/cpp_int.hpp>
+using namespace boost::multiprecision;
+using int128 = int128_t;
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define rep(i, n) for (long long int i = 0; i < (n); ++i)
+#define rep2(i, k, n) for (long long int i = (k); i < (n); ++i)
+using ll = long long;
+using vint = vector<int>;
+using vll = vector<ll>;
+using vvint = vector<vector<int>>;
+using vvll = vector<vector<ll>>;
+
+const ll INF = (ll)2e18 + 9;
+// const int INF = (int)2e9 + 7;
+
+template <typename T>
+void chmin(T& a, T b) {
+    a = min(a, b);
+}
+template <typename T>
+void chmax(T& a, T b) {
+    a = max(a, b);
+}
+
+template <typename T>
+void print(vector<T> v) {
+    int n = v.size();
+    rep(i, n) {
+        if (i == 0)
+            cout << v[i];
+        else
+            cout << ' ' << v[i];
+    }
+    cout << endl;
+}
+
+void yesno(bool x) {
+    cout << (x ? "Yes" : "No") << '\n';
+}
+
+void Yes() {
+    yesno(true);
+}
+
+void No() {
+    yesno(false);
+}
+
+// ceil(a/b)
+ll ceil(ll a, ll b) {
+    return (a + b - 1) / b;
+}
+
+// floor(a/b)
+ll floor(ll a, ll b) {
+    return a / b;
+}
+
+void solve();
+
+int main() {
+    solve();
+    return 0;
+}
+
+void solve() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    auto cal = []() -> void {
+        ll N;
+        cin >> N;
+        vll P(N);
+        rep(i, N) cin >> P[i];
+
+        vector<pair<ll, ll>> ps;
+        rep(i, N) {
+            ps.emplace_back(i + 1, P[i]);
+        }
+        reverse(all(ps));
+
+        ll l = -1, r = INF;
+        ll ans = 0;
+        for (auto [before, after] : ps) {
+            if (before < after) continue;
+            if (before < l && r < after) continue;
+            ans++;
+            l = before, r = after;
+        }
+        cout << ans << '\n';
+    };
+
+    int t;
+    cin >> t;
+    rep(i, t) cal();
+}
